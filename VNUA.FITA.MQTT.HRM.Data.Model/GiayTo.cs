9c0a7cd9e58da4b2dc93102f9da5a786caec648b
VanQuyet5H <phamhoangh7@gmail.com>
2022-10-28 07:58:15 +0700

    HRM

---
+++ VNUA.FITA.MQTT.HRM.Data.Model/GiayTo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace VNUA.FITA.MQTT.HRM.Data.Model
+{
+    /// <summary>
+    /// Nguyễn Đình Thuyết - K64CNPM - 646372
+    /// </summary>
+    [Table("GiayTo")]
+    public class GiayTo
+    {
+        [Key]
+        public int MaGT { get; set; }
+
+        [StringLength(100)]
+        public string TenGT { get; set; }
+        public byte[] Anh { get; set; }
+        [StringLength(20)]
+        public string MaNhanVien { get; set; }
+
+        [ForeignKey("MaNhanVien")]
+        public NhanVien NhanViens { get; set; }
+    }
+}
---
+++ VNUA.FITA.MQTT.HRM.Data.Model/Luong.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace VNUA.FITA.MQTT.HRM.Data.Model
+{
+    /// <summary>
+    /// Nguyễn Đình Thuyết - K64CNPM - 646372
+    /// </summary>
+    [Table("Luong")]
+    public class Luong
+    {
+        [Key]
+        public int MaLuong { get; set; }
+
+        public double LCoBan { get; set; }
+
+        public int HeSo { get; set; }
+
+        public double PhiPhat { get; set; }
+
+        public double PhiThue { get; set; }
+
+        public double LThucNhan { get; set; }
+
+        public DateTime ThoiGian { get; set; }
+
+        [StringLength(20)]
+        public string MaNhanVien { get; set; }
+
+        [ForeignKey("MaNhanVien")]
+        public NhanVien NhanViens { get; set; }
+    }
+}
